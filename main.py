e82cb3f1075b5f8e6034a869db7fa7a4c046a653
kuvsamja <kuv.cizal.2011@gmail.com>
2025-08-15 17:24:14 +0200

    fixed floor and ceiling clipping bug and separeted all inputs into a function

--- main.py
+++ main.py
@@ -95,6 +95,28 @@ def playerMovement(player_speed, player_a, player_l):
             player_l = player_l - 360
     return dx, dy, dz, player_a, player_l
 
+def misc_inputs():
+    # Zoom
+    global focal_lenght, focal_lenght_old, zoom
+    focal_lenght = focal_lenght_old
+    if buttons[pygame.K_c]:
+        focal_lenght = zoom
+
+    # ms delay
+    global delta_time, last_tick
+    delta_time = pygame.time.get_ticks() - last_tick
+    last_tick = pygame.time.get_ticks()
+    if buttons[pygame.K_m]:
+        print(delta_time)
+
+    # Load map
+    if buttons[pygame.K_KP_ENTER]:
+        print("yes")
+
+def inputs():
+    global dx, dy, dz, player_a, player_l
+    dx, dy, dz, player_a, player_l = playerMovement(player_speed, player_a, player_l)
+    misc_inputs()
 #_______________________________________________________________________________________
 
 class Walls():
@@ -103,6 +125,10 @@ class Walls():
     x2 = 0
     y2 = 0
     color = (0, 0, 0)
+    wall_texture = 0
+    u = 0
+    v = 0
+    shade = 0
 
 class Sectors():
     wall_start = 0
@@ -114,26 +140,37 @@ class Sectors():
     surface = 0
     z1 = 0
     z2 = 0
+    surface_texture = 0
+    surface_scale = 0
 
-W = [Walls() for i in range(100)]
-S = [Sectors() for i in range(100)]
-v1 = 0
-v2 = 0
-for s in range(map.SECTOR_NUM):
-    S[s].wall_start = map.loadSectors()[v1 + 0]
-    S[s].wall_end = map.loadSectors()[v1 + 1]
-    S[s].z1 = map.loadSectors()[v1 + 2]
-    S[s].z2 = map.loadSectors()[v1 + 3] - map.loadSectors()[v1 + 2]
-    S[s].color1 = map.loadSectors()[v1 + 4]
-    S[s].color2 = map.loadSectors()[v1 + 5]
-    v1 = v1 + 6
-    for w in range(S[s].wall_start, S[s].wall_end):
-        W[w].x1 = map.loadWalls()[v2+0]
-        W[w].y1 = map.loadWalls()[v2+1]
-        W[w].x2 = map.loadWalls()[v2+2]
-        W[w].y2 = map.loadWalls()[v2+3]
-        W[w].color = map.loadWalls()[v2+4]
-        v2 = v2 + 5
+class TextureMaps():
+    texture_width = 0
+    texture_height = 0
+    texture_name = "placeholder"
+
+def loadMap():
+    global textures, W, S
+    textures = [TextureMaps() for i in range(64)]
+    W = [Walls() for i in range(256)]
+    S = [Sectors() for i in range(128)]
+    v1 = 0
+    v2 = 0
+    for s in range(map.SECTOR_NUM):
+        S[s].wall_start = map.loadSectors()[v1 + 0]
+        S[s].wall_end = map.loadSectors()[v1 + 1]
+        S[s].z1 = map.loadSectors()[v1 + 2]
+        S[s].z2 = map.loadSectors()[v1 + 3] - map.loadSectors()[v1 + 2]
+        S[s].color1 = map.loadSectors()[v1 + 4]
+        S[s].color2 = map.loadSectors()[v1 + 5]
+        v1 = v1 + 6
+        for w in range(S[s].wall_start, S[s].wall_end):
+            W[w].x1 = map.loadWalls()[v2+0]
+            W[w].y1 = map.loadWalls()[v2+1]
+            W[w].x2 = map.loadWalls()[v2+2]
+            W[w].y2 = map.loadWalls()[v2+3]
+            W[w].color = map.loadWalls()[v2+4]
+            v2 = v2 + 5
+loadMap()
 
 def clipBehindPlayer(x1, y1, z1, x2, y2, z2):
     da = y1
@@ -148,7 +185,7 @@ def clipBehindPlayer(x1, y1, z1, x2, y2, z2):
     z1 = z1 + s * (z2 - z1)
     return x1, y1, z1
 
-def drawWall(x1, x2, b1, b2, t1, t2, color, s):
+def drawWall(x1, x2, b1, b2, t1, t2, color, s, w, frontBack):
     pixel_array = pygame.PixelArray(scaled_surface)
     dyb = b2 - b1
     dyt = t2 - t1
@@ -157,37 +194,47 @@ def drawWall(x1, x2, b1, b2, t1, t2, color, s):
         dx = 1
     xs = x1
     # Clip x
-    if x1 < 1:  x1 = 1
-    if x2 < 1:  x1 = 1
-    if x1 > width - 1:  x1 = width - 1
-    if x2 > width - 1:  x2 = width - 1
+    if x1 < 0:  x1 = 0
+    if x2 < 0:  x1 = 0
+    if x1 > width:  x1 = width
+    if x2 > width:  x2 = width
+
     for x in range(x1, x2):
         y1 = dyb * (x - xs) / dx + b1
         y2 = dyt * (x - xs) / dx + t1
         # Clip y
-        if y1 < 1:  y1 = 1
-        if y2 < 1:  y2 = 1
-        if y1 > height - 1:  y1 = height - 1
-        if y2 > height - 1:  y2 = height - 1
-        if S[s].surface == 1:
-            S[s].surf[x] = y1
-            continue
-        if S[s].surface == 2:
-            S[s].surf[x] = y2
-            continue
-        if S[s].surface == -1:
-            pixel_array[x, int(S[s].surf[x]):int(y1)] = S[s].color1
-            # for y in range(int(S[s].surf[x]), int(y1)):
-            #     pixel_array[x, y] = S[s].color1
-        if S[s].surface == -2:
-            pixel_array[x, int(y2):int(S[s].surf[x])] = S[s].color2
-            # for y in range(int(y2), int(S[s].surf[x])):
-            #     pixel_array[x, y] = S[s].color2
+        if y1 < 0:  y1 = 0
+        if y2 < 0:  y2 = 0
+        if y1 > height:  y1 = height
+        if y2 > height:  y2 = height
+        # if S[s].surface == 1:
+        #     S[s].surf[x] = y1
+        #     continue
+        # if S[s].surface == 2:
+        #     S[s].surf[x] = y2
+        #     continue
+        # if S[s].surface == -1:
+        #     pixel_array[x, int(S[s].surf[x]):int(y1)] = S[s].color1
+        # if S[s].surface == -2:
+        #     pixel_array[x, int(y2):int(S[s].surf[x])] = S[s].color2
+
+        # Front walls
+        if frontBack == 0:
+            if S[s].surface == 1:
+                S[s].surf[x] = y1
+            if S[s].surface == 2:
+                S[s].surf[x] = y2
+            pixel_array[x, int(y1):int(y2)] = color
+        # Back walls
+        if frontBack == 1:
+            if S[s].surface == 1:
+                y2 = S[s].surf[x]
+                pixel_array[x, int(y1):int(y2)] = S[s].color1
+            if S[s].surface == 2:
+                y1 = S[s].surf[x]
+                pixel_array[x, int(y1):int(y2)] = S[s].color2
 
 
-
-
-        pixel_array[x, int(y1):int(y2)] = color
     del pixel_array
     
 
@@ -205,21 +252,29 @@ def draw3D():
                 st = S[w]
                 S[w] = S[w + 1]
                 S[w + 1] = st
+    
     for s in range(map.SECTOR_NUM):
         if player_z < S[s].z1:
             S[s].surface = 1
+            cycles = 2
+            for i in range(width):
+                S[s].surf[i] = height
         elif player_z > S[s].z2 + S[s].z1:
             S[s].surface = 2
+            cycles = 2
+            for i in range(width):
+                S[s].surf[i] = 0
         else:
             S[s].surface = 0
+            cycles = 1
 
-        for loop in range(2):
+        for frontBack in range(cycles):
             S[s].d = 0
             for w in range (S[s].wall_start, S[s].wall_end):
                 # Point world location (no tilting)
                 x1 = W[w].x1 - player_x; y1 = W[w].y1 - player_y
                 x2 = W[w].x2 - player_x; y2 = W[w].y2 - player_y
-                if loop == 0:
+                if frontBack == 1:
                     swp = x2
                     x2 = x1
                     x1 = swp
@@ -260,10 +315,8 @@ def draw3D():
                 world_x[3] = int(world_x[3] * focal_lenght / world_y[3] + width / 2); world_y[3] = int(world_z[3] * focal_lenght / world_y[3] + height / 2)
                 
                 # Draw points
-                drawWall(world_x[0], world_x[1], world_y[0], world_y[1], world_y[2], world_y[3], W[w].color, s)
+                drawWall(world_x[0], world_x[1], world_y[0], world_y[1], world_y[2], world_y[3], W[w].color, s, w, frontBack)
             S[s].d = S[s].d / (S[s].wall_end - S[s].wall_start)
-            S[s].surface = S[s].surface * -1
-
 
 running = True
 while running:
@@ -271,23 +324,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     buttons = pygame.key.get_pressed()
-    dx, dy, dz, player_a, player_l = playerMovement(player_speed, player_a, player_l)
+    inputs()
     player_x = player_x + dx; player_y = player_y + dy; player_z = player_z + dz
     scaled_surface.fill(BLACK)
 
-    # Zoom
-    focal_lenght = focal_lenght_old
-    if buttons[pygame.K_c]:
-        focal_lenght = zoom
+
 
     draw3D()
     window.blit(pygame.transform.scale(scaled_surface, (width * scale, height * scale)), (0, 0))
     pygame.display.update()
 
-    delta_time = pygame.time.get_ticks() - last_tick
-    last_tick = pygame.time.get_ticks()
-    
-    if buttons[pygame.K_m]:
-        print(delta_time)
 
     pygame.time.delay(1000 // fps)
